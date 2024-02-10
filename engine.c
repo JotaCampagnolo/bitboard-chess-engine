368ab22a4418bebb0c86790f50250f33f91528cb
João M. Campagnolo <jota.campagnolo@gmail.com>
2024-02-10 13:02:22 -0300

    :sparkles: feat: implementing RUDIMENTARY EVALUATION (positional piece scores).

--- engine.c
+++ engine.c
@@ -2172,6 +2172,72 @@ int material_score[12] = {
 		-10000 // black KING
 };
 
+// Pawn positional score:
+const int pawn_scores[64] = {
+		90, 90, 90, 90, 90, 90, 90, 90,
+		30, 30, 30, 40, 40, 30, 30, 30,
+		20, 20, 20, 30, 30, 30, 20, 20,
+		10, 10, 10, 20, 20, 10, 10, 10,
+		5, 5, 10, 20, 20, 5, 5, 5,
+		0, 0, 0, 5, 5, 0, 0, 0,
+		0, 0, 0, -10, -10, 0, 0, 0,
+		0, 0, 0, 0, 0, 0, 0, 0};
+
+// Knight positional score:
+const int knight_scores[64] = {
+		-5, 0, 0, 0, 0, 0, 0, -5,
+		-5, 0, 0, 10, 10, 0, 0, -5,
+		-5, 5, 20, 20, 20, 20, 5, -5,
+		-5, 10, 20, 30, 30, 20, 10, -5,
+		-5, 10, 20, 30, 30, 20, 10, -5,
+		-5, 5, 20, 10, 10, 20, 5, -5,
+		-5, 0, 0, 0, 0, 0, 0, -5,
+		-5, -10, 0, 0, 0, 0, -10, -5};
+
+// Bishop positional score:
+const int bishop_scores[64] = {
+		0, 0, 0, 0, 0, 0, 0, 0,
+		0, 0, 0, 0, 0, 0, 0, 0,
+		0, 0, 0, 10, 10, 0, 0, 0,
+		0, 0, 10, 20, 20, 10, 0, 0,
+		0, 0, 10, 20, 20, 10, 0, 0,
+		0, 10, 0, 0, 0, 0, 10, 0,
+		0, 30, 0, 0, 0, 0, 30, 0,
+		0, 0, -10, 0, 0, -10, 0, 0};
+
+// Rook positional score:
+const int rook_scores[64] = {
+		50, 50, 50, 50, 50, 50, 50, 50,
+		50, 50, 50, 50, 50, 50, 50, 50,
+		0, 0, 10, 20, 20, 10, 0, 0,
+		0, 0, 10, 20, 20, 10, 0, 0,
+		0, 0, 10, 20, 20, 10, 0, 0,
+		0, 0, 10, 20, 20, 10, 0, 0,
+		0, 0, 10, 20, 20, 10, 0, 0,
+		0, 0, 0, 20, 20, 0, 0, 0};
+
+// King positional score:
+const int king_scores[64] = {
+		0, 0, 0, 0, 0, 0, 0, 0,
+		0, 0, 5, 5, 5, 5, 0, 0,
+		0, 5, 5, 10, 10, 5, 5, 0,
+		0, 5, 10, 20, 20, 10, 5, 0,
+		0, 5, 10, 20, 20, 10, 5, 0,
+		0, 0, 5, 10, 10, 5, 0, 0,
+		0, 5, 5, -5, -5, 0, 5, 0,
+		0, 0, 5, 0, -15, 0, 10, 0};
+
+// Mirror positional score tables for opposite side:
+const int mirror_scores[128] = {
+		a1, b1, c1, d1, e1, f1, g1, h1,
+		a2, b2, c2, d2, e2, f2, g2, h2,
+		a3, b3, c3, d3, e3, f3, g3, h3,
+		a4, b4, c4, d4, e4, f4, g4, h4,
+		a5, b5, c5, d5, e5, f5, g5, h5,
+		a6, b6, c6, d6, e6, f6, g6, h6,
+		a7, b7, c7, d7, e7, f7, g7, h7,
+		a8, b8, c8, d8, e8, f8, g8, h8};
+
 // Position evaluation:
 static inline int evaluate()
 {
@@ -2195,6 +2261,42 @@ static inline int evaluate()
 			square = get_ls1b_index(bitboard);
 			// Score material weights:
 			score += material_score[piece];
+			// Score positional weights:
+			switch (piece)
+			{
+			// Evaluate white pieces:
+			case P:
+				score += pawn_scores[square];
+				break;
+			case N:
+				score += knight_scores[square];
+				break;
+			case B:
+				score += bishop_scores[square];
+				break;
+			case R:
+				score += rook_scores[square];
+				break;
+			case K:
+				score += king_scores[square];
+				break;
+			// Evaluate black pieces:
+			case p:
+				score -= pawn_scores[mirror_scores[square]];
+				break;
+			case n:
+				score -= knight_scores[mirror_scores[square]];
+				break;
+			case b:
+				score -= bishop_scores[mirror_scores[square]];
+				break;
+			case r:
+				score -= rook_scores[mirror_scores[square]];
+				break;
+			case k:
+				score -= king_scores[mirror_scores[square]];
+				break;
+			}
 			// Pop LS1B:
 			pop_bit(bitboard, square);
 		}
